dde1f39031d83a85eaed1d4a46f9efbad9c5ce04
Muhammet ARIKAYA <radiationhere@gmail.com>
2018-05-28 17:20:15 +0300

    first commit

---
+++ components/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer>
+    aisit our website for more documentation : <a href="https://nuxtjs.org" target="_blank">nuxtjs.org</a>
+  </footer>
+</template>
---
+++ nuxt.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack')
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'starter',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  css: ['~/assets/css/main.scss'],
+  build: {
+    vendor: [
+      'jquery',
+      'bootstrap',
+      'axios'
+    ],
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+      })
+    ]
+  },
+  plugins: ['~plugins/bootstrap.js']
+}
